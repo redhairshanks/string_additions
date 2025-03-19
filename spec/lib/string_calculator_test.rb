23a8ae6e40f2a7ecc2744b87aef4e7a8ac0de551
Pravin Patil <pravinpatil3009@gmail.com>
2025-03-19 11:10:06 +0530

    returns number for single number in input

--- spec/lib/string_calculator_test.rb
+++ spec/lib/string_calculator_test.rb
@@ -5,5 +5,9 @@ describe StringCalculator do
     it 'returns 0 for empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it 'returns number for single number' do 
+      expect(StringCalculator.add('1')).to eq(1)
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,11 @@
 class StringCalculator
   def self.add(input)
     0 if input.empty?
+    numbers = extract_numbers(input)
+    numbers.sum
+  end
+
+  def extract_numbers(input)
+    input.split(',').map(&:to_i)
   end
 end
